33b8364d78effaa5b3748805d9d0d0d943830a77
rainiera <rainier@utexas.edu>
2017-09-16 18:06:37 -0400

    Add data wrangling stuff

---
+++ deeplearning/oneoff.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+h1b_data = pd.read_csv('data.csv')
+desired_cols = ['JOB_TITLE',
+        'EMPLOYER_NAME',
+        'WORKSITE_STATE',
+        'WORKSITE_CITY',
+        'WAGE_RATE_OF_PAY_FROM']
+
+# Remove non-yearly-rate WAGE_RATE_OF_PAY_FROM records
+a = h1b_data[desired_cols]
+
+a['WAGE_RATE_OF_PAY_FROM'] = a['WAGE_RATE_OF_PAY_FROM'].apply(
+    lambda x: int(x.replace(',','')[:-3]))
+b = a[~(a['WAGE_RATE_OF_PAY_FROM'] < 10000)]
+b.head()
+
+b.to_csv('h1b.tsv', sep='\t')
