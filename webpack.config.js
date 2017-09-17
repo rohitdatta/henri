1a96d3aba7fdd62b0efd024c18f6e8ce2d3930ea
evonne <evonne_ng@utexas.edu>
2017-09-16 23:50:45 -0400

    added deckgl framework for data visualization

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,42 @@
-var config = {
-   entry: './main.js',
-	
-   output: {
-      path:'/',
-      filename: 'index.js',
-   },
-	
-   devServer: {
-      inline: true,
-      port: 8080
-   },
-	
-   module: {
-      loaders: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-				
-            query: {
-               presets: ['es2015', 'react']
-            }
-         }
-      ]
-   }
-}
+// NOTE: To use this example standalone (e.g. outside of deck.gl repo)
+// delete the local development overrides at the bottom of this file
 
-module.exports = config;
+// avoid destructuring for older Node version support
+const resolve = require('path').resolve;
+const webpack = require('webpack');
+
+const CONFIG = {
+  entry: {
+    app: resolve('./app.js')
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [{
+      // Compile ES2015 using buble
+      test: /\.js$/,
+      loader: 'buble-loader',
+      include: [resolve('.')],
+      exclude: [/node_modules/],
+      options: {
+        objectAssign: 'Object.assign'
+      }
+    }]
+  },
+
+  resolve: {
+    alias: {
+      // From mapbox-gl-js README. Required for non-browserify bundlers (e.g. webpack):
+      'mapbox-gl$': resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
+    }
+  },
+
+  // Optional: Enables reading mapbox token from environment variable
+  plugins: [
+    new webpack.EnvironmentPlugin(['MapboxAccessToken'])
+  ]
+};
+
+// This line enables bundling against src in this repo rather than installed deck.gl module
+module.exports = env => env ? require('../webpack.config.local')(CONFIG)(env) : CONFIG;
