260c421fd7af2bb67e7f09559686462c61071534
rainiera <rainier@utexas.edu>
2017-09-16 20:49:40 -0400

    Parametrize prior and add seed

--- deeplearning/oneoff_supervised.py
+++ deeplearning/oneoff_supervised.py
@@ -18,12 +18,14 @@ b = b.dropna()
 c = b.copy()
 TECHIES = set(['SOFTWARE', 'PROGRAMMER', 'DEVELOPER', 'ENGINEER'])
 from numpy.random import rand
+TECHIE_PRIOR = 0.95
 # 95% of the time, TECHIES will be identified as a HENRY
-is_a_techie = lambda job_title: any([word in TECHIES for word in job_title.upper().strip().split()]) and rand() <= 0.95
+is_a_techie = lambda job_title: any([word in TECHIES for word in job_title.upper().strip().split()]) and rand() <= TECHIE_PRIOR
 c['HENRY'] = c['JOB_TITLE'].map(is_a_techie)
 
 
 c.to_csv('h1b_sampler.tsv', sep='\t', index=False)
-
-c.sample(frac=0.01, replace=True).to_csv('h1b_henry_sampler_oo.tsv', sep='\t', index=False, header=False)
+import random
+random.seed(1337)
+c.sample(frac=0.01, replace=True).to_csv('h1b_henry_sampler_{}_prior.tsv'.format(TECHIE_PRIOR), sep='\t', index=False, header=False)
 
